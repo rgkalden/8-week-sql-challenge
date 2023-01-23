ebeead74d73a0c59e6e70c521acf850b05756c39
rgkalden <rgkalden@gmail.com>
2023-01-23 09:57:43 -0700

    Case Study 6 - questions part 2

--- Case Study 6 - Clique Bait/question_answers.sql
+++ Case Study 6 - Clique Bait/question_answers.sql
@@ -9,8 +9,38 @@ SET search_path = clique_bait;
 SELECT COUNT(DISTINCT user_id) FROM users;
 
 --     How many cookies does each user have on average?
+
+WITH num_cookies_user AS (
+	SELECT 
+		user_id,
+		COUNT(cookie_id) AS num_cookies
+	FROM users
+	GROUP BY user_id
+)
+
+SELECT AVG(num_cookies) FROM num_cookies_user;
+
 --     What is the unique number of visits by all users per month?
+
+SELECT
+	EXTRACT(MONTH FROM event_time) AS month,
+	COUNT(DISTINCT visit_id)
+FROM users u
+JOIN events e ON u.cookie_id = e.cookie_id
+GROUP BY month
+ORDER BY month;
+
 --     What is the number of events for each event type?
+
+SELECT
+	e.event_type,
+	event_name,
+	COUNT(e.event_type)
+FROM events e
+JOIN event_identifier i ON e.event_type = i.event_type
+GROUP BY e.event_type, event_name
+ORDER BY e.event_type;
+
 --     What is the percentage of visits which have a purchase event?
 --     What is the percentage of visits which view the checkout page but do not have a purchase event?
 --     What are the top 3 pages by number of views?
