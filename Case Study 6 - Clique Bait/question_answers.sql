8f2931e6ffbf3919b39d2471c34130afb565d648
rgkalden <rgkalden@gmail.com>
2023-02-05 12:48:01 -0700

    Case Study 6 - Start Part 4

--- Case Study 6 - Clique Bait/question_answers.sql
+++ Case Study 6 - Clique Bait/question_answers.sql
@@ -343,3 +343,101 @@ SELECT
 FROM product_info;
 
 
+-- 4. Campaigns Analysis
+
+-- Generate a table that has 1 single row for every unique visit_id record and has the following columns:
+
+--     user_id
+--     visit_id
+--     visit_start_time: the earliest event_time for each visit
+--     page_views: count of page views for each visit
+--     cart_adds: count of product cart add events for each visit
+--     purchase: 1/0 flag if a purchase event exists for each visit
+--     campaign_name: map the visit to a campaign if the visit_start_time falls between the start_date and end_date
+--     impression: count of ad impressions for each visit
+--     click: count of ad clicks for each visit
+--     (Optional column) cart_products: a comma separated text value with products added to the cart sorted by the order they were added to the cart (hint: use the sequence_number)
+
+DROP TABLE IF EXISTS campaign_analysis;
+CREATE TABLE campaign_analysis AS (
+	WITH events_users_pages AS (
+		SELECT
+			visit_id,
+			user_id,
+			events.page_id,
+			page_hierarchy.page_name,
+			events.event_type,
+			event_identifier.event_name,
+			sequence_number,
+			event_time
+		FROM events
+		JOIN users ON users.cookie_id = events.cookie_id
+		JOIN event_identifier ON event_identifier.event_type = events.event_type
+		JOIN page_hierarchy ON page_hierarchy.page_id = events.page_id
+		ORDER BY user_id, visit_id, sequence_number
+	),
+
+	cart_items_added AS (
+		SELECT
+			visit_id,
+			STRING_AGG(page_name, ', ' ORDER BY sequence_number) AS cart_items
+		FROM events
+		JOIN page_hierarchy ON page_hierarchy.page_id = events.page_id
+		WHERE event_type = 2
+		GROUP BY visit_id
+	)
+
+	SELECT
+		events_users_pages.visit_id,
+		user_id,
+		MIN(event_time) AS visit_start_time,
+		SUM(
+			CASE
+				WHEN event_name = 'Page View' THEN 1
+				ELSE 0
+			END
+		) AS page_views,
+		SUM(
+			CASE
+				WHEN event_name = 'Add to Cart' THEN 1
+				ELSE 0
+			END
+		) AS cart_adds,
+		MAX(
+			CASE
+				WHEN event_name = 'Purchase' THEN 1
+				ELSE 0
+			END
+		) AS purchase,
+		(SELECT
+			campaign_name
+		FROM campaign_identifier
+		WHERE MIN(event_time) BETWEEN start_date AND end_date) AS campaign_name,
+		SUM(
+			CASE
+				WHEN event_name = 'Ad Impression' THEN 1
+				ELSE 0
+			END
+		) AS impression,
+		cart_items	
+	FROM events_users_pages
+	LEFT JOIN cart_items_added ON cart_items_added.visit_id = events_users_pages.visit_id
+	GROUP BY events_users_pages.visit_id, user_id, cart_items
+	ORDER BY user_id, visit_id
+);
+
+SELECT * FROM campaign_analysis;
+
+-- Use the subsequent dataset to generate at least 5 insights for the Clique Bait team 
+-- bonus: prepare a single A4 infographic that the team can use for their management reporting sessions, 
+-- be sure to emphasise the most important points from your findings.
+
+-- Some ideas you might want to investigate further include:
+
+--     Identifying users who have received impressions during each campaign period and comparing each metric 
+--	   		with other users who did not have an impression event
+--     Does clicking on an impression lead to higher purchase rates?
+--     What is the uplift in purchase rate when comparing users who click on a campaign impression versus 
+--     		users who do not receive an impression? What if we compare them with users who just an impression but do not click?
+--     What metrics can you use to quantify the success or failure of each campaign compared to eachother?
+
